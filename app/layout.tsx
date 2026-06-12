6cc215d9f604e7012c3c80177901e799ce30748c
Amir Karimi <amk9978@gmail.com>
2026-06-12 20:18:57 +0400

    fix: expand layout container for full-width background

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,19 +46,19 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        "text-black bg-white dark:text-white dark:bg-black",
+        "text-neutral-900 bg-white dark:text-neutral-100 dark:bg-black",
         GeistSans.variable,
         GeistMono.variable,
       )}
     >
-      <body className="antialiased max-w-3xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className="antialiased">
+        <div className="max-w-3xl mx-4 mt-8 lg:mx-auto flex-auto min-w-0 flex flex-col px-2 md:px-0">
           <Navbar />
-          {children}
+          <main className="flex-auto flex flex-col gap-10">{children}</main>
           <Footer />
           <Analytics />
           <SpeedInsights />
-        </main>
+        </div>
       </body>
     </html>
   );
